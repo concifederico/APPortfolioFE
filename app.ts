59d6ba88a6bee2afc20db463ef0188054ff1c27c
Federico Conci <concifederico@gmail.com>
2022-09-04 10:28:35 -0300

    Primer JS

---
+++ app.ts
@@ -0,0 +1,12 @@
+function dologin() {
+  if (document.getElementById("btnlogin").innerHTML == "Login") {
+    document.getElementById("btnlogin").innerHTML = "Logout";
+    document.getElementById("btnlogin").className = "btn btn btn-danger";
+    document.getElementById("expedit").className = "card cvisible";
+    console.log("Apreto");
+  } else {
+    document.getElementById("btnlogin").innerHTML = "Login";
+    document.getElementById("btnlogin").className = "btn btn btn-primary";
+    document.getElementById("expedit").className = "card cinvisible";
+  }
+}
